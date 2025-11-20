7179b0e38b9a7ca42d764e3cdc46f8055bda7741
YeDemirkiran <yedemirkiran27@gmail.com>
2025-11-20 16:32:37 +0300

    Add null check to ft_strlen

--- src/string/ft_strlen.c
+++ src/string/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: yademirk <yademirk@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 11:59:51 by yademirk          #+#    #+#             */
-/*   Updated: 2025/11/16 20:34:03 by yademirk         ###   ########.fr       */
+/*   Updated: 2025/11/20 16:31:38 by yademirk         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *c)
 {
 	size_t	count;
 
+	if (c == NULL)
+		return (0);
 	count = 0;
 	while (c[count])
 		count++;
